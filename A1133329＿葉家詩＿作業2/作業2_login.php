94b4df2ae6f0da2ff450496a94c0368f804ee52b
家詩 <a1133329@mail.nuk.edu.tw>
2026-04-16 23:09:15 +0700

    Add files via upload

---
+++ A1133329＿葉家詩＿作業2/作業2_login.php
@@ -0,0 +1,10 @@
+
+<form action="作業2_logincheck.php" method="POST">
+ID:<br>
+<input type="text" name="uID"><br>
+Password:<br>
+<input type="password" name="uPWD"><br>
+<input type="submit">
+
+</form>
+
